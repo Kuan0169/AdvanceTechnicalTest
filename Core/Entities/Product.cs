7a56fe3b82104427ea2edbe119934504c3ac2143
Kuan <h0169888@gmail.com>
2025-06-17 20:32:15 +0200

    created Entities folder and entities(Product)

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Test.Core.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public required Guid ProductId { get; set; }
+
+    }
+}

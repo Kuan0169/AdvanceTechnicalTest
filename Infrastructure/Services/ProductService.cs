aaa0db45701dd0154cc1c9ff8da0f08d20262bbd
Kuan <h0169888@gmail.com>
2025-06-18 20:59:36 +0200

    feat(ProductService): -add Interface IProductService -add GetAllAsync() -add GetByIdAsync(Guid id) -add CreateAsync(ProductModel productModel) -add UpdateAsync(Guid id, ProductModel productModel) -add DeleteAsync(Guid id)

--- Infrastructure/Services/ProductService.cs
+++ Infrastructure/Services/ProductService.cs
@@ -1,13 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using MyCompany.Test.Core;
+using MyCompany.Test.Core.Entities;
+using MyCompany.Test.Infrastructure.Models;
+
 
 namespace MyCompany.Test.Infrastructure.Services
 {
-    public class ProductService
+    public interface IProductService
     {
+        Task<IEnumerable<ProductDto>> GetAllAsync();
+        Task<ProductDto?> GetByIdAsync(Guid id);
+        Task<ProductDto> CreateAsync(ProductModel productModel);
+        Task UpdateAsync(Guid id, ProductModel productModel);
+        Task DeleteAsync(Guid id);
+    }
 
+    public class ProductService : IProductService
+    {
+        private readonly ProductContext context;
+        public ProductService(ProductContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllAsync()
+        {
+            return await context.Product.Select(p => new ProductDto
+            {
+                Id = p.ProductId,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                CreatedAt = p.CreatedAt
+            }).ToListAsync();
+        }
+        public async Task<ProductDto> GetByIdAsync(Guid id)
+        {
+            var product = await context.Product.Where(p => p.ProductId == id).Select(c => new ProductDto
+            {
+                Id = c.ProductId,
+                Name = c.Name,
+                Price = c.Price,
+                Description = c.Description,
+                CreatedAt = c.CreatedAt,
+            }).FirstOrDefaultAsync();
+
+            if (product == null)
+            {
+                throw new Exception("Product not found");
+            }
+            return product;
+        }
+
+        public async Task<ProductDto> CreateAsync(ProductModel productModel)
+        {
+            var product = new Product
+            {
+                ProductId = Guid.NewGuid(),
+                Name = productModel.Name,
+                Description = productModel.Description,
+                Price = productModel.Price,
+                CreatedAt = productModel.CreatedAt,
+            };
+            context.Product.Add(product);
+            await context.SaveChangesAsync();
+            var productDto = new ProductDto
+            {
+                Id = product.ProductId,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                CreatedAt = product.CreatedAt
+            };
+            return productDto;
+        }
+
+        public async Task UpdateAsync(Guid id, ProductModel productModel)
+        {
+            var product = await context.Product.Where(c => c.ProductId == id).FirstOrDefaultAsync();
+            if (product == null)
+            {
+                throw new Exception("Product not found");
+            }
+
+            product.ProductId = id;
+            product.Name = productModel.Name;
+            product.Description = productModel.Description;
+            product.Price = productModel.Price;
+            product.CreatedAt = productModel.CreatedAt;
+
+            context.Product.Update(product);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var product = await context.Product.Where(c => c.ProductId == id).FirstOrDefaultAsync();
+            if (product == null)
+            {
+                throw new Exception("Product not found");
+            }
+            context.Product.Remove(product);
+            await context.SaveChangesAsync();
+        }
     }
 }
+
+

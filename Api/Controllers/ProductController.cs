5b399c95050c4f8a6f12acac0870e6d20ce0ea50
Kuan <h0169888@gmail.com>
2025-06-18 21:21:53 +0200

    feat(ProductContorller): -add ProductController

---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Test.Infrastructure.Models;
+using MyCompany.Test.Infrastructure.Services;
+
+namespace MyCompany.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController(IProductService productService) : ControllerBase
+{
+    private readonly IProductService _productService = productService;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var products = await _productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpGet]
+    [Route("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        try
+        {
+            var product = await _productService.GetByIdAsync(id);
+            return Ok(product);
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message == "Product not found")
+                return NotFound();
+        }
+        return StatusCode(500, "Internal server error");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ProductModel productModel)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        var created = await _productService.CreateAsync(productModel);
+        return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+    }
+
+    [HttpPut]
+    [Route("{id:guid}")]
+    public async Task<IActionResult> UpdateAsync(Guid id, ProductModel productModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        try
+        {
+            await _productService.UpdateAsync(id, productModel);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+
+    [HttpDelete]
+    [Route("{id:guid}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        try
+        {
+            await _productService.DeleteAsync(id);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message == "Product not found")
+            {
+                return NotFound();
+            }
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+}
